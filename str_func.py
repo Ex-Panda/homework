ff92148cba7fc32ca9dda8fc2e4efa0b9a98914a
frausonya <frausonya131@gmail.com>
2023-07-16 18:16:54 +0300

    title and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def uppers(a):
+    '''функция заглавных букв'''
     return a.upper()
+
+def title(stroka):
+    '''функция увеличения первых букв'''
+    str_list = stroka.split(' ')
+    list_2 = []
+    for s in str_list:
+        list_2.append(s.title())
+
+    return ' '.join(list_2)
