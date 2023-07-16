5fc80f9c260f4919b92ac03e28c7419750191bbc
frausonya <frausonya131@gmail.com>
2023-07-16 18:24:32 +0300

    tru title and docstring

--- str_func.py
+++ str_func.py
@@ -4,9 +4,4 @@ def uppers(a):
 
 def title(stroka):
     '''функция увеличения первых букв'''
-    str_list = stroka.split(' ')
-    list_2 = []
-    for s in str_list:
-        list_2.append(s.title())
-
-    return ' '.join(list_2)
+    return stroka.title()
